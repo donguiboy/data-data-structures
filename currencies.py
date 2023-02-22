7d4a7af4621822ccb6a944ca9d6eb2670e67f310
gonzalo lara <gonzaloflara@gmail.com>
2023-02-22 16:01:25 -0300

    all tests passed

--- currencies.py
+++ currencies.py
@@ -1,11 +1,29 @@
 # pylint: disable=missing-docstring
 
 # TODO: add some currency rates
-RATES = {}
+RATES = {
+    "USDEUR": 0.85,
+    "GBPEUR": 1.13,
+    "CHFEUR": 0.86,
+    "EURGBP": 0.885
+    }
 
 def convert(amount, currency):
     """returns the converted amount in the given currency
     amount is a tuple like (100, "EUR")
     currency is a string
+
+    amount = (100,"EUR")
+    currency = ("EURGBP")
+                    convert(100, "EURGBP")
+                        100 * EURGBP => 88,5
     """
-    pass  # YOUR CODE HERE
+   # values = amount[0]
+   # change = amount[1]+currency
+    #RATES.get(change, None)
+    for key, value in RATES.items():
+        if amount[1] == key[0:3] and currency == "EUR":
+            return round(value * amount[0])
+        if amount[1] == key[0:3] and currency == "GBP":
+            return round(value * amount[0])
+    return None
